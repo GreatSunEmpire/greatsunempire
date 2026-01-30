9e39af8b282bb61e46ad13999c19b8b0e62ef780
idiotickkyle9923 <kilealt@gmail.com>
2026-01-30 10:02:21 +0800

    UPDATE README.MD

--- javascript/pop-list.js
+++ javascript/pop-list.js
@@ -1,9 +1,9 @@
 // Example data, could be loaded from a JSON file or API
 const popularLinks = [
-  { title: "與納斯共和國之合併公告", url: "link1.html" },
-  { title: "大陽聯合帝國對索洛夫當局制裁的正式回應", url: "link2.html" },
-  { title: "本帝國與索洛夫社會主義聯邦共和國斷交聲明", url: "link3.html" },
-  { title: "對索洛夫社會主義聯邦共和國不當行為之抗議聲明", url: "#" },
+  { title: "與納斯共和國之合併公告", url: "https://greatsunempire.github.io/greatsunempire/post.html?category=PMOfficeA&id=1" },
+  { title: "大陽聯合帝國對索洛夫當局制裁的正式回應", url: "https://greatsunempire.github.io/greatsunempire/post.html?category=ForeignMinistryA&id=3" },
+  { title: "本帝國與索洛夫社會主義聯邦共和國斷交聲明", url: "https://greatsunempire.github.io/greatsunempire/post.html?category=ForeignMinistryA&id=2" },
+  { title: "對索洛夫社會主義聯邦共和國不當行為之抗議聲明", url: "https://greatsunempire.github.io/greatsunempire/post.html?category=ForeignMinistryA&id=1"},
 ];
 
 const list = document.getElementById('popular-list');
